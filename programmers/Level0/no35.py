2da28245197e222a64b56d33cf853a391e758d8a
zoanna <103236992+choara5442@users.noreply.github.com>
2024-05-03 17:29:49 +0900

    :memo: 5/3 code commit

---
+++ programmers/Level0/no35.py
@@ -0,0 +1,15 @@
+#programmers Leve0 No.15 : 수 조작하기 
+#링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181926
+def solution(n, control):
+    answer = 0
+    for c in control :
+        if c == "w" :
+            n += 1
+        elif c == "s" :
+            n -= 1
+        elif c == "d" :
+            n += 10 
+        elif c == "a" :
+            n -= 10
+    answer = n
+    return answer
---
+++ programmers/Level0/no36.py
@@ -0,0 +1,18 @@
+#programmers Leve0 No.15 : 수 조작하기 
+#링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181925
+def solution(numLog):
+    answer = ''
+    l = len(numLog) - 1
+    for n in range(l,0, -1) : #뒤에서 부터 계산
+        temp = numLog[n]-numLog[n-1]
+        if temp == 1 :
+            answer+= "w"
+        elif temp == -1 :
+            answer+="s"
+        elif temp == 10 :
+            answer+="d"
+        elif temp == -10 :
+            answer+="a"
+
+    answer = ''.join(reversed(answer))
+    return answer
