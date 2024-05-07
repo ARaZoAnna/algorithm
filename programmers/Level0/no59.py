3d53f98639ce5f1c524323966c5503cbb5e8221f
zoanna <103236992+choara5442@users.noreply.github.com>
2024-05-07 21:00:34 +0900

    :memo: 5/7 code

--- programmers/Level0/no59.py
+++ programmers/Level0/no59.py
@@ -1,3 +1,13 @@
 #No.59 : 문자 개수 세기 
 #링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181902
 
+def solution(my_string):
+    answer = [0] * 52
+    
+    for s in my_string :
+        if ord('A') <= ord(s) <= ord('Z') :
+            answer[ord(s)-ord('A')] += 1
+        else :
+            answer[ord(s)-ord('a')+26] += 1
+            
+    return answer
---
+++ programmers/Level0/no60.py
@@ -0,0 +1,3 @@
+#No.60 : 배열 만들기1
+#링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181901
+
