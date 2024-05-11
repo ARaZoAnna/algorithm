2e97dec2744b14a6130092c283dee14a800bf924
zoanna <103236992+choara5442@users.noreply.github.com>
2024-05-12 00:30:03 +0900

    :memo: 5/11 code

--- programmers/Level0/no89.py
+++ programmers/Level0/no89.py
@@ -1,3 +1,23 @@
 #No.89 : 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기 
 #링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181872
 
+def solution(myString, pat):
+    answer = ''
+    n = len(myString)
+    m = len(pat)
+    last = pat[m-1]
+    for idx in reversed(range(n)) :
+        if myString[idx] == last :
+            ix = idx
+            for inx in reversed(range(m)) :
+                if myString[ix] != pat[inx] :
+                    ix = 0
+                    break
+                else : 
+                    ix -= 1
+            if ix == 0 :
+                continue
+            answer += myString[0:idx+1]
+            break
+        
+    return answer
---
+++ programmers/Level0/no90.py
@@ -0,0 +1,3 @@
+#No.90 : 문자열이 몇 번 등장하는지 세기 
+#링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181871
+
