a7bd36b58527c6a097d5d5e2d08940d1e3bd0c95
zoanna <103236992+choara5442@users.noreply.github.com>
2024-05-19 23:15:00 +0900

    :memo: 5/19 code

---
+++ programmers/Level0/120807.py
@@ -0,0 +1,9 @@
+#숫자 비교하기 : https://school.programmers.co.kr/learn/courses/30/lessons/120807
+
+def solution(num1, num2):
+    answer = 0
+    if num1 == num2 :
+        answer = 1
+    else :
+        answer = -1
+    return answer
