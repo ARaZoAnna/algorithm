eb8c739f9336005ecf1d91cb610b991ffdfff842
ARa <103236992+ARaZoAnna@users.noreply.github.com>
2025-02-07 23:44:10 +0900

    2월7일 / sec2. String(문자열) / 01-06 중복문자제거
    https://cote.inflearn.com/contest/10/problem/01-06

---
+++ JAVA/COTE/Sec2-String/중복문자제거.java
@@ -0,0 +1,36 @@
+//https://cote.inflearn.com/contest/10/problem/01-06
+
+import java.util.*;
+
+class Test {
+    public String solutions(String str){
+        String answer = "";
+        //아스키 코드 이용
+        boolean[] check = new boolean[26];
+        char[] c = str.toCharArray();
+        
+        for (char x : c){
+            int num = x-'a';
+            if(check[num]) {// 중복된 문자가 있다면
+                continue;
+            }
+            else{ // 중복된 문자가 없다면
+                check[num] = true;
+                answer += x;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Test T = new Test();
+        Scanner in = new Scanner(System.in);
+        String str = in.next();
+
+        System.out.println(T.solutions(str));
+        in.close();
+        
+    }
+
+}
