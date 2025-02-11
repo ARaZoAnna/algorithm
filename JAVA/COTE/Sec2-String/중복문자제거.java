47f1d4015855a2c98990e6ed81ab601e0ba7b490
ARa <103236992+ARaZoAnna@users.noreply.github.com>
2025-02-11 14:28:11 +0900

    2월11일 / sec2. String(문자열) / 01-06 중복문자제
    https://cote.inflearn.com/contest/10/problem/01-06

--- JAVA/COTE/Sec2-String/중복문자제거.java
+++ JAVA/COTE/Sec2-String/중복문자제거.java
@@ -1,22 +1,27 @@
-//https://cote.inflearn.com/contest/10/problem/01-06
-
 import java.util.*;
 
 class Test {
     public String solutions(String str){
         String answer = "";
         //아스키 코드 이용
-        boolean[] check = new boolean[26];
-        char[] c = str.toCharArray();
+        // boolean[] check = new boolean[26];
+        // char[] c = str.toCharArray();
         
-        for (char x : c){
-            int num = x-'a';
-            if(check[num]) {// 중복된 문자가 있다면
-                continue;
-            }
-            else{ // 중복된 문자가 없다면
-                check[num] = true;
-                answer += x;
+        // for (char x : c){
+        //     int num = x-'a';
+        //     if(check[num]) {// 중복된 문자가 있다면
+        //         continue;
+        //     }
+        //     else{ // 중복된 문자가 없다면
+        //         check[num] = true;
+        //         answer += x;
+        //     }
+        // }
+
+        //charAt과 indexOf 사용
+        for (int i = 0; i < str.length(); i++){
+            if (i == str.indexOf(str.charAt(i))){
+                answer += str.charAt(i);
             }
         }
 
