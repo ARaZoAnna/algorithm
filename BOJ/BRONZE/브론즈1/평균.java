747e5be2680f94682d6e986bbf44c02533784178
ARa <103236992+ARaZoAnna@users.noreply.github.com>
2025-04-09 16:50:57 +0900

    4월8일 / Do it! 코테 강의 Sec2  / 브론즈1 - 평균(1546)
    https://www.acmicpc.net/problem/1546

---
+++ BOJ/BRONZE/브론즈1/평균.java
@@ -0,0 +1,23 @@
+//https://www.acmicpc.net/problem/1546
+
+import java.util.*;
+
+class Test {
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        String str = in.next();
+
+        char[] cArr = str.toCharArray();
+        int answer = 0;
+
+        for(int i = 0; i < cArr.length; i++){
+            answer += (cArr[i] - '0');
+        }
+        System.out.println(answer);
+        
+        in.close();      
+    }
+}
