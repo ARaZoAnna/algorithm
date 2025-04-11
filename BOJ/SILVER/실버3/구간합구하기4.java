ece16e03b0c3148a0727b3abc3e0c81d05dabbc3
ARa <103236992+ARaZoAnna@users.noreply.github.com>
2025-04-11 14:28:43 +0900

    4월11일 / Do it! 코테 강의 Sec2  / 실버3 - 구간합구하기4(11659)
    https://www.acmicpc.net/problem/11659

--- BOJ/SILVER/실버3/구간합구하기4.java
+++ BOJ/SILVER/실버3/구간합구하기4.java
@@ -32,3 +32,39 @@ class Test {
         in.close();      
     }
 }
+
+
+
+//====================================================== 
+
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+class Test {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        int suNo = Integer.parseInt(stringTokenizer.nextToken());
+        int quizNo = Integer.parseInt(stringTokenizer.nextToken());
+
+        long[] S = new long[suNo+1];
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        for(int i = 0; i < suNo; i++){
+            S[i] = S[i-1] + Integer.parseInt(stringTokenizer.nextToken());
+        }
+
+        for(int q = 0; q < quizNo; q++){
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int i = Integer.parseInt(stringTokenizer.nextToken());
+            int j = Integer.parseInt(stringTokenizer.nextToken());
+            System.out.println(S[j] - S[i-1]);
+        }
+           
+    }
+}
