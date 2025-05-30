b1b6ee7542ba68a8999366e8d7866f3ae76bd9f3
ARa <103236992+ARaZoAnna@users.noreply.github.com>
2025-05-30 15:31:39 +0900

    5월30일 / sec4. Two Pointers, Sliding Windows(On) / 03-01 두 배열 합치기 - 2번째풀이
    https://cote.inflearn.com/contest/10/problem/03-01

--- JAVA/COTE/Sec4-TwoPointers_SlidingWindows/두배열합치기.java
+++ JAVA/COTE/Sec4-TwoPointers_SlidingWindows/두배열합치기.java
@@ -1,49 +1,34 @@
 //https://cote.inflearn.com/contest/10/problem/03-01
 
 import java.util.*;
-
-class Test {
-    
-    public int[] solutions(int n, int[] arrn, int m, int[] arrm){
-        int[] answer = new int[n+m];
-      
-        int idn = 0, idm = 0, i = 0;
-        while(true){
-            if(idn >= n && idm >= m){
-                break;
-            }else if(idn >= n || arrn[idn] >= arrm[idm]){
-                answer[i] = arrm[idm]; 
-                idm++;
-            } else{
-                answer[i] = arrn[idn];
-                idn++;
-            }
-            i++;
-        }
-        
-        return answer;    
-    }
-
-    public static void main(String[] args){
-        Test T = new Test();
+class Main {
+	public static void main(String[] args){
+		Main T = new Main();
         Scanner in = new Scanner(System.in);
-        
+
         int n = in.nextInt();
-        int[] arrn = new int[n];
+        int[] arrN = new int[n];
         for(int i = 0; i < n; i++){
-            arrn[i] = in.nextInt();
+            arrN[i] = in.nextInt();
         }
-
         int m = in.nextInt();
-        int[] arrm = new int[m];
-        for(int j = 0; j < m; j++){
-            arrm[j] = in.nextInt();
+        int[] arrM = new int[m];
+        for(int i = 0; i < m; i++){
+            arrM[i] = in.nextInt();
         }
 
-        for(int num : T.solutions(n, arrn, m, arrm)){
-            System.out.print(num + " ");
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        int nidx = 0;
+        int midx = 0;
+
+        while(nidx < n && midx < m){
+            if(arrN[nidx] <= arrM[midx]) answer.add(arrN[nidx++]) ;
+            else if(arrM[midx] < arrN[nidx]) answer.add(arrM[midx++]);
         }
-        
-        in.close();      
+        while(nidx < n) answer.add(arrN[nidx++]);
+        while(midx < m) answer.add(arrM[midx++]);
+
+        for(int t : answer) System.out.print(t + " ");
     }
 }
