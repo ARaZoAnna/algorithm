7565cd499ef9fc39db866d0c64b5fd656753167a
ARa <103236992+ARaZoAnna@users.noreply.github.com>
2026-01-02 20:34:00 +0900

    26년01월02일 / 리트코드(leetcode) / 232. 스택을 이용한 큐 구현
    https://leetcode.com/problems/implement-queue-using-stacks/

---
+++ JAVA/leetcode/ImplementQueueusingStacks.java
@@ -0,0 +1,48 @@
+//https://leetcode.com/problems/implement-queue-using-stacks/description/
+
+import java.util.*;
+class MyQueue {
+    Stack<Integer> inStack;
+    Stack<Integer> outStack;
+    public MyQueue() {
+        inStack = new Stack<>();
+        outStack = new Stack<>();
+    }
+    
+    public void push(int x) {
+        inStack.add(x);
+    }
+    
+    public int pop() {
+        if(outStack.isEmpty()){
+            while(!inStack.isEmpty()){
+                outStack.add(inStack.pop());
+            }
+        }
+        return outStack.pop();
+        
+    }
+    
+    public int peek() {
+        if(outStack.isEmpty()){
+            while(!inStack.isEmpty()){
+                outStack.add(inStack.pop());
+            }
+        }
+        return outStack.peek();
+    }
+    
+    public boolean empty() {
+        if(inStack.isEmpty() && outStack.isEmpty()) return true;
+        else return false;
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
