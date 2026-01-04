1944d47718673dd4fe6963bc40bc6a7e57700eab
ARa <103236992+ARaZoAnna@users.noreply.github.com>
2026-01-04 17:40:48 +0900

    26년01월04일 / 리트코드(leetcode) / 1. 투 섬 : Map 사용 풀이

---
+++ JAVA/leetcode/TwoSum2.java
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/two-sum/
+
+import java.util.*;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] answer = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            map.put(nums[i], i);          
+        }
+        for(int i = 0; i < nums.length; i++){
+            int check = 0;
+            if(nums[i] <= target) check = target - nums[i];
+            if(map.containsKey(check) && map.get(check) != i){
+                answer[0] = i;
+                answer[1] = map.get(check);
+                break;
+            }
+        }
+        return answer;
+    }
+}
