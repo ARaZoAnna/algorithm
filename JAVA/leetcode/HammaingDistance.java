f0414ef900985af342d89646ee7e1349f69f5aa0
ARa <103236992+ARaZoAnna@users.noreply.github.com>
2026-01-06 20:12:14 +0900

    26년01월06일 / 리트코드(leetcode) / 461. 해밍 거리

---
+++ JAVA/leetcode/HammaingDistance.java
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/hamming-distance/
+
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int xor = x ^ y;
+        int cnt = 0;
+        for(int i = 0; i < 32; i++){
+            cnt += (xor >> i) & 1;
+        }
+        return cnt;
+    }
+}
