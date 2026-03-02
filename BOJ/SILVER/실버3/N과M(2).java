55eda837173917cc50a40ebdcb2ac94069ba25e8
ARa <103236992+ARaZoAnna@users.noreply.github.com>
2026-03-02 16:39:04 +0900

    26년03월02일 / N과 M(2)(15650)
    https://www.acmicpc.net/problem/15650

--- BOJ/SILVER/실버3/N과M(2).java
+++ BOJ/SILVER/실버3/N과M(2).java
@@ -4,20 +4,15 @@ import java.util.*;
   
 public class Main {
   static int n, m;
-  static int[] ans, check;
+  static int[] ans;
   public void dfs(int L, int idx){
     if(L == m){
       for(int a : ans) System.out.print(a + " ");
       System.out.println();
     }else{
       for(int i = idx; i<= n; i++){
-        if(check[i-1] == 0){
-          ans[L] = i;
-          check[i-1] = 1;
-          dfs(L+1, i);
-          check[i-1] = 0;
-        }
-        
+        ans[L] = i;
+        dfs(L+1, i+1);
       }
     }
 
@@ -28,7 +23,6 @@ public class Main {
     Scanner in=new Scanner(System.in);
     n = in.nextInt();
     m = in.nextInt();
-    check = new int[n];
     ans = new int[m];
     T.dfs(0, 1);
     return ;
